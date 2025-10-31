dfc1c45f37ba8fade75e16973629d4bffc7c91b2
kazybaevv <bekzatkazybaev05@gmail.com>
2025-10-31 19:58:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/cityguide/PlaceAdapter.java
@@ -0,0 +1,10 @@
+package com.example.cityguide;
+
+import java.util.ArrayList;
+
+import models.Place;
+
+public class PlaceAdapter {
+    public PlaceAdapter(MainActivity mainActivity, ArrayList<Place> places, Object place) {
+    }
+}
---
+++ app/src/main/java/models/Place.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.io.Serializable;
+
+
+public class Place implements Serializable {
+    private String title;
+    private String description;
+    private String imageUrl;
+
+
+    public Place(String title, String description, String imageUrl) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public String getImageUrl() { return imageUrl; }
+}
